c48a200ac7fc847b2ed5465f0c0ef29dc751c7da
MakMax123 <100467867+MakMax123@users.noreply.github.com>
2023-03-26 11:28:06 +0100

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,7 @@
+a = 33
+b = "33"
+if b > a:
+    print("B is greater than A")
+elif b == a:
+    print("A and B are equal")
+
---
+++ test2.py
@@ -0,0 +1,6 @@
+def my_function(fname):
+    print(fname +"refsnes")
+
+my_function("emily")
+my_function("tobias")
+my_function("Linus")
